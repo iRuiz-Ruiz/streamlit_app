d5c4f21ed164a22203957af69b7bae445c6e33b9
M. Isabel Ruiz Ruiz <95575812+iRuiz-Ruiz@users.noreply.github.com>
2023-07-22 18:26:54 -0500

    Update myfile.py

--- myfile.py
+++ myfile.py
@@ -7,5 +7,6 @@ st.title('Titulo del Proyecto')
 option = st.selectbox('¿Cómo desea ser contactado/a?',('Email','Teléfono','Whatsapp'))
 st.write('Seleccionó:', option)
 
-m=np.random.randn(1000,2) / [50, 50] + [37.76,-122.4],columns=['lat', 'lon']
-df = pd.DataFrame(m)
+n = st.slider("n", 5,100,step=1)
+chart_data = pd.DataFrame(np.random.randn(n),columns=['data'])
+st.line_chart(chart_data)
