9a14240b1aeb0b5c60c658678ff5051271d2b42c
M. Isabel Ruiz Ruiz <95575812+iRuiz-Ruiz@users.noreply.github.com>
2023-07-22 18:24:09 -0500

    Update myfile.py

--- myfile.py
+++ myfile.py
@@ -7,4 +7,6 @@ st.title('Titulo del Proyecto')
 option = st.selectbox('¿Cómo desea ser contactado/a?',('Email','Teléfono','Whatsapp'))
 st.write('Seleccionó:', option)
 
-df = pd.DataFrame(np.random.randn(1000,2) / [50,50] + [37.76,-122.4],columns=['lat','lon'])
+df = pd.DataFrame(
+  np.random.randn(1000,2) / [50, 50] + [37.76,-122.4],
+  columns=['lat', 'lon'])
