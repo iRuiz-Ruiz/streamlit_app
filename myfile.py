9bfef53aa5abe85b9b7a77efb71045d6f89b2801
M. Isabel Ruiz Ruiz <95575812+iRuiz-Ruiz@users.noreply.github.com>
2023-07-22 16:48:43 -0500

    Update myfile.py

--- myfile.py
+++ myfile.py
@@ -5,3 +5,7 @@ import numpy as np
 n = st.slider("n", 5,100,step=1)
 chart_data = pd.DataFrame(np.random.randn(n),columns=['data'])
 st.line_chart(chart_data)
+
+df = pd.DataFrame(
+  np.random.randn(1000,2) / [50,50] + [37.76, -122.4],
+  columns=['lat','lon'])
