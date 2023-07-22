3a1d45b92050ff0c900ec1e9dbf639237caf3ca0
M. Isabel Ruiz Ruiz <95575812+iRuiz-Ruiz@users.noreply.github.com>
2023-07-22 18:28:18 -0500

    Add files via upload
    esperando que funcione

--- myfile.py
+++ myfile.py
@@ -7,3 +7,8 @@ st.title('Titulo del Proyecto')
 n = st.slider("n", 5,100,step=1)
 chart_data = pd.DataFrame(np.random.randn(n),columns=['data'])
 st.line_chart(chart_data)
+
+option = st.selectbox('¿Cómo desea ser contactado/a?','Email','Teléfono','Whatsapp'))
+st.write('Seleccionó:', option)
+
+df = pd.DataFrame(np.random.randn(1000,2) / [50,50] + [37.76, -122.4], columns=['lat','lon'])
