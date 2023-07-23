e9fc60430c1d97f7341ec7c357ec7a003b769daa
M. Isabel Ruiz Ruiz <95575812+iRuiz-Ruiz@users.noreply.github.com>
2023-07-22 19:21:56 -0500

    Update myfile.py

--- myfile.py
+++ myfile.py
@@ -10,3 +10,6 @@ df = pd.DataFrame(np.random.randn(1000,2) / [50,50] + [37.76, -122.4], columns=[
 #Mostrar mapa
 st.map(df)
 
+n = st.slider("n", 5,100,step=1)
+chart_data = pd.DataFrame(np.random.randn(n),columns=['data'])
+st.line_chart(chart_data)
