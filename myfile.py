36617700da7772c0a212abc65d904753fde3eb47
M. Isabel Ruiz Ruiz <95575812+iRuiz-Ruiz@users.noreply.github.com>
2023-07-22 19:21:10 -0500

    Update myfile.py

--- myfile.py
+++ myfile.py
@@ -3,6 +3,10 @@ import pandas as pd
 import numpy as np
 
 st.title('Titulo del Proyecto')
+
+num = st.slider("num", 0, 100, step=1)
+st.write("El numero ingresado es {}".format(num))
 df = pd.DataFrame(np.random.randn(1000,2) / [50,50] + [37.76, -122.4], columns=['lat','lon'])
 #Mostrar mapa
 st.map(df)
+
